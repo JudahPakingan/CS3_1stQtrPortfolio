2aa0c09fd4f06429394c463f4737f5a4b172246c
JudahPakingan <b2029jepakingan@pshs.edu.ph>
2025-08-28 12:10:28 +0800

    qweqwe

---
+++ public/anything.js
@@ -0,0 +1,21 @@
+// gma - 7
+// abs-cbn - 2
+// ibc -13
+// ptv - 4
+// tv5 - 5
+
+const rl = require('readline')
+const rli = rl.createInterface({
+    input: process.stdin,
+})
+
+rli.on('line', give_channel_name)
+
+function give_channel_name(num){
+    switch(num) {
+        case '7';
+             console.log("gma")
+         case '7';
+             console.log("gma")
+    }
+}
