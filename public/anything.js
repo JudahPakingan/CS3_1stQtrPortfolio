cc0a0e9b1661c2af1ebfeecd5bdfcd7d88f34b69
JudahPakingan <b2029jepakingan@pshs.edu.ph>
2025-09-01 11:06:46 +0800

    commit

--- public/anything.js
+++ public/anything.js
@@ -1,21 +1,42 @@
-// gma - 7
-// abs-cbn - 2
-// ibc -13
-// ptv - 4
-// tv5 - 5
+// HELLO - ENGLISH
+// MABUHAY - TAGALOG
+// HOLA - SPANISH
+// HALLO - GERMAN
+// BONJOUR - FRENCH
+// CIAO - ITALIAN
+// ZDRAVSTUJTE - RUSSIAN
 
 const rl = require('readline')
 const rli = rl.createInterface({
     input: process.stdin,
 })
 
-rli.on('line', give_channel_name)
+rli.on('line', give_greeting_language)
 
-function give_channel_name(num){
+function give_greeting_language(num){
     switch(num) {
-        case '7';
-             console.log("gma")
-         case '7';
-             console.log("gma")
+        case 'ENGLISH':
+            console.log("HELLO")
+            break
+        case 'TAGALOG':
+            console.log("MABUHAY")
+            break
+        case 'SPANISH':
+            console.log("HOLA")
+            break
+        case 'GERMAN':
+            console.log("HALLO")
+            break
+        case 'FRENCH':
+            console.log("BONJOUR")
+            break
+        case 'ITALIAN':
+            console.log("CIAO")
+            break
+        case 'RUSSIAN':
+            console.log("ZDRAVSTUJTE")
+            break
+        default:
+            console.log("UNKNOWN")
     }
 }
